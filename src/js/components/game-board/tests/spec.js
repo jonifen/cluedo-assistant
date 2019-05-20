1312d0dc67f9c8bc2c0c2dd85210bb314ffb8a48
Jon Cain <jonifen@users.noreply.github.com>
2019-05-20 19:33:26 +0100

    Mock out primitive default export from entities-config.js

--- src/js/components/game-board/tests/spec.js
+++ src/js/components/game-board/tests/spec.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { render, cleanup } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import GameBoard from '../index';
+import Entities from '../../../entities-config';
+
+jest.mock('../../../entities-config', () => {
+  return [{ "entity":"colonel-mustard", "name":"Col. Mustard", "category":"suspect" }];
+});
 
 describe('<GameBoard />', () => {
   afterEach(cleanup);
-
+  
   it('Should render without crashing', () => {
     render(<GameBoard />);
+  });
+  
+  it('should render 1 entity when the Entities file contains 1 item', () => {
+    const { getByTestId } = render(<GameBoard />);
+    expect(getByTestId('entity-image')).toBeInTheDocument();
   });
 });
